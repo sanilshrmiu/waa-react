3601e5f7f6cfbfdc10b6cb118a4dea949f2e3347
sanilshrmiu <129837713+sanilshrmiu@users.noreply.github.com>
2023-06-15 14:46:15 -0500

    Lab-8 init

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,23 +1,36 @@
 import React, { useEffect, useState } from "react";
 import Post from "./Post";
+import axios from "axios";
 
 const Posts = ({ firstPostName }) => {
-  const [Posts, setPosts] = useState([]);
+  const [data, setData] = useState([]);
 
-  useEffect(() => {
-    setPosts([
-      {
-        id: 111,
-        title: firstPostName ? firstPostName : "React",
-        author: "John Doe",
-      },
-      { id: 112, title: "React2", author: "John Doe" },
-    ]);
-  }, [firstPostName]);
+  //   useEffect(() => {
+  //     setData([
+  //       {
+  //         id: 111,
+  //         title: firstPostName ? firstPostName : "React",
+  //         author: "John Doe",
+  //       },
+  //       { id: 112, title: "React2", author: "John Doe" },
+  //     ]);
+  //   }, [firstPostName]);
+
+  const fetchData = () => {
+    console.log("call");
+    axios
+      .get("http://localhost:9990/api/v1/users")
+      .then((response) => console.log(response))
+      .catch((error) => {
+        console.log(error.message);
+      });
+  };
+
+  useEffect(() => fetchData(), []);
 
   return (
     <div>
-      {Posts.map((p) => {
+      {data.map((p) => {
         return <Post key={p.id} post={p} />;
       })}
     </div>
